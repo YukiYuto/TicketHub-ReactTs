0f23eccada67fc363f9ac762689eaed15453fcc4
PhamQuan2003 <bestzed12345ff@gmail.com>
2025-07-14 14:21:56 +0700

    Add Authorization header with Bearer token to Axios request"

--- src/utils/axios/axiosInstance.ts
+++ src/utils/axios/axiosInstance.ts
@@ -1,11 +1,24 @@
 import axios from "axios";
 
 import { HOST_API_KEY } from "@utils/apiUrl/gobalConfig";
+import { getJwtTokenSession } from "@/auth/auth.utilities";
 
 // Creating an instance of Axios with a base URL
 const axiosInstance = axios.create({
   baseURL: HOST_API_KEY,
 });
+
+// Adding a request interceptor to include Authorization header
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const { accessToken } = getJwtTokenSession();
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 // Adding a response interceptor to the Axios instance
 axiosInstance.interceptors.response.use(
